19a81210906dd7c43d7db44744e7e6399667fffe
e.mukaev <e.mukaev@tbank.ru>
2025-10-17 13:40:46 +0500

    storybook fixes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,66 @@
+import { dirname, join } from 'path'
+import path from 'node:path'
 import type { StorybookConfig } from '@storybook/react-webpack5';
+
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-styling-webpack'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-styling-webpack',
+    {
+      name: getAbsolutePath('@storybook/addon-styling-webpack'),
+      options: {
+        rules: [
+          {
+            test: /\.module\.css$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                  modules: {
+                    localIdentName: '[path][name]__[local]',
+                  },
+                },
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  postcssOptions: {
+                    config: path.resolve(__dirname, '..', 'postcss.config.js'),
+                  },
+                },
+              },
+            ],
+          },
+          {
+            test: /\.css$/,
+            exclude: /\.module\.css$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                },
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  postcssOptions: {
+                    config: path.resolve(__dirname, '..', 'postcss.config.js'),
+                  },
+                },
+              },
+            ],
+          }
+        ]
+      }
+    }
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,13 @@ declare module '*.css' {
     const classes: Record<string, string>
     export default classes
   }
+
+  declare module '*.svg' {
+    const content: string
+    export default content
+  }
+
+  declare module '*.png' {
+    const content: string
+    export default content
+  }
